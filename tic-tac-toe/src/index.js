54f7c45255505049772ef950075cddd6a3b45866
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-07-14 12:00:17 -0300

    Final do projeto

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -54,7 +54,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -65,6 +65,7 @@ class Game extends React.Component {
         history: history.concat([{
             squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
         });
     }
@@ -77,9 +78,8 @@ class Game extends React.Component {
     }
 
     render() {
-
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
         if (winner) {
